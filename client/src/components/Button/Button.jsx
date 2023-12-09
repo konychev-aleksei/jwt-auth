9adb200b090d3276457ea4553967bd3952e77999
Aleksei Konychev <konychevaleksei@yandex.ru>
2023-12-09 11:47:35 +0400

    speedup

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,7 +1,8 @@
+import { memo } from "react";
 import style from "./button.module.scss";
 
-export default ({ children, ...rest }) => (
+export default memo(({ children, ...rest }) => (
   <button {...rest} className={style.button}>
     {children}
   </button>
-);
+));
